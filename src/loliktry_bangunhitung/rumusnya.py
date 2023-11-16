9b04d80f83dc5c254e3b5aad2709212485dbccfd
Liliek Triyono <d4010133@yahoo.com>
2023-11-16 20:23:07 +0700

    merubah penamaan kelas class pada rumusnya.py pada versi 0.1.3

--- src/loliktry_bangunhitung/rumusnya.py
+++ src/loliktry_bangunhitung/rumusnya.py
@@ -1,15 +1,18 @@
 import math
 # Rumus bangun
 
-class rumusnya:
+class Rumusnya:
+    def __init__(self):
+        return None
+
     def luas_persegipanjang(self, a, b):
         return a * b
+
     def luas_lingkaran(self, radius):
         return math.pi * radius ** 2
 
     def keliling_lingkaran(self, radius):
         return 2 * math.pi * radius
-
 
     # Rumusa hitungan
     def tambah(self, a, b):
