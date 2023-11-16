72924dd2fc27059c089a6e4e0ba2ce0b333dd6fb
Liliek Triyono <d4010133@yahoo.com>
2023-11-16 13:21:28 +0700

    Initial commit

---
+++ LBangun.py
@@ -0,0 +1,7 @@
+import math
+
+def luas_lingkaran(radius):
+    return math.pi * radius ** 2
+
+def keliling_lingkaran(radius):
+    return 2 * math.pi * radius
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LDasProX',
+    version='0.1.0',
+    author='Liliek Triyono',
+    author_email='liliek.triyono@gmail.com',
+    description='Library ini digunakan hanya untuk kesenangan semata tidak untuk sesuatu yang serius. Jika ada kesamaan nama dan peran itu hanya kebetulan semata.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
